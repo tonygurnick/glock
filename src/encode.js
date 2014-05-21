fa856f83d6c0fa5c385bddc28cc05c70a5667d47
tonygurnick <tony.gurnick@gmail.com>
2014-05-21 17:01:13 +0100

    refactor

--- src/encode.js
+++ src/encode.js
@@ -2,11 +2,8 @@
 	var PNG = require('pngjs').PNG,
 	fs = require("fs");
 
-	module.exports=function( imageBuffer, filename, cb ){
-
-		text = imageBuffer.toString("utf-8");
-
-		var len =  Math.ceil( text.length / 4 );
+	var encode = function( dataString ){
+		var len =  Math.ceil( dataString.length / 4 );
 		var width = height = Math.ceil( len/Math.sqrt(len) );
 
 		var img = new PNG({ width: width, height: height, filterType: 4 });
@@ -16,15 +13,24 @@
 			for (var x = 0; x < img.width; x++) {
 				var idx = (img.width * y + x) << 2;
 
-				img.data[idx]   = text.charCodeAt(idx);
-				img.data[idx+1] = text.charCodeAt(idx+1);
-				img.data[idx+2] = text.charCodeAt(idx+2);
-				img.data[idx+3] = text.charCodeAt(idx+3);
+				img.data[idx]   = dataString.charCodeAt(idx);
+				img.data[idx+1] = dataString.charCodeAt(idx+1);
+				img.data[idx+2] = dataString.charCodeAt(idx+2);
+				img.data[idx+3] = dataString.charCodeAt(idx+3);
 			}
 		}
-		if ( cb ) {
-			img.pack().pipe(fs.createWriteStream(filename).on("finish", function (err) {
-				cb(err, filename)
+		return img;
+	};
+
+	module.exports=function( dataBuffer, filename, cb ){
+
+		var img = encode( dataBuffer.toString("utf-8") );
+
+		if ( filename && cb ) {
+
+			img.pack().pipe( fs.createWriteStream(filename).on("finish", function ( err  ) {
+
+				cb(err, img );
 			}));
 		} else {
 			return img.pack();
